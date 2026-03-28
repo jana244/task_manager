91516a10db0ef0c2ed24bbfe011d869f75d5bb86
Jana <jana@email.com>
2026-03-28 15:17:11 +0100

    task_manager_2

--- TaskManager.py
+++ TaskManager.py
@@ -1,80 +1,73 @@
 import sys
 
-def main(tasks):
-    if tasks is None:
-        tasks = []
+task_list = []
+
+def main():
 
     while True:
         print("1: Přidat nový úkol")
         print("2: Zobrazit všechny úkoly")
         print("3: Odstranit úkol")
         print("4: Konec programu")
-        hodnota = input("Vyberte možnost (1-4): ")
-        if hodnota == "4":
+        choice = input("Vyberte možnost (1-4): ").strip()
+        if choice == "4":
             print("Konec programu.")
-            break  
-        elif hodnota == "1":
-            tasks= pridat_ukol(tasks)
-            main(tasks)
-        elif hodnota == "2":
-            seznam_ukolu(tasks)
-            print("")
-            main(tasks)
-        elif hodnota == "3":
-            tasks= odstranit_ukol(tasks)
-            main(tasks)
+            break
+        elif choice == "1":
+            add_task()
+        elif choice == "2":
+            show_tasks()
+        elif choice == "3":
+            remove_task()
         else:
-            print("mimo rozsah")
-            main(tasks)
-        sys.exit()
+            print("Chyba: Vybraná možnost není v nabídce. Zadejte číslo 1 až 4.")
 
-def pridat_ukol(tasks):
-    print()
-    task= input("Zadejte název úkolu: ")
-    description= input("Zadejte popis úkolu: ")
-    if task =="" or description =="":
-        print("prázdné políčko")
-        pridat_ukol(tasks)
+def add_task():
+    global task_list
+    task_name = input("\nZadejte název úkolu: ")
+    task_description = input("Zadejte popis úkolu: ")
+    if task_name == "" and task_description == "":
+        print("Chyba: Název úkolu ani popis nesmí být prázdný.")
+        add_task()
+    elif task_name == "":
+        print("Chyba: Název úkolu nesmí být prázdný.")
+        add_task()
+    elif task_description == "":
+        print("Chyba: Popis úkolu nesmí být prázdný.")
+        add_task()
     else:
-        tasks.append(task + " - " + description)
+        task_list.append({"name": task_name, "description": task_description})
+        print("Úkol " + task_name + " byl přidán\n")
 
-    print("Úkol " + task + " byl přidán")
-    print()
-    return tasks
-
-def seznam_ukolu(tasks):
-    print()
-    if len(tasks) == 0:
-        print("Nejsou žádné úkoly")
-        print()
+def show_tasks():
+    global task_list
+    if len(task_list) == 0:
+        print("\nNejsou žádné úkoly\n")
         return
-    print("Seznam úkolů: ")
-    for i in range(len(tasks))  :
-        print(str(i+1) + ". " + tasks[i])
+    print("\nSeznam úkolů: ")
+    for i in range(len(task_list)):
+        print(str(i+1) + ". " + task_list[i]["name"] + " - " + task_list[i]["description"])
     print()
 
-def odstranit_ukol(tasks):
-    if len(tasks) == 0:
-        print("Nejsou žádné úkoly. Není co smazat.")
-        print()
-        return tasks
+def remove_task():
+    global task_list
+    if len(task_list) == 0:
+        print("Nejsou žádné úkoly. Není co smazat.\n")
+        return
 
-    seznam_ukolu(tasks)    
-#    del_task=int(input("Zadejte číslo úkolu, které chcete odstranit: "))
-    del_task=input("Zadejte číslo úkolu, které chcete odstranit: ")
-    if not del_task.isdigit():
-        print("špatně zadaný")
-        return tasks
-    if int(del_task) == 0:
-        return tasks
-    elif int(del_task) > len(tasks):
-        print("mimo rozsah")
-    else: 
-        task = tasks[int(del_task)-1]
-        print("Úkol " + task.split(" -")[0] + " byl odstraněn.")
-        del tasks[int(del_task)-1]
+    show_tasks()
+    task_to_delete = input("Zadejte číslo úkolu, které chcete odstranit: ")
+    if not task_to_delete.isdigit():
+        print("Chyba: Musíte zadat číslo úkolu, který chcete odstranit.")
+        return
+    if int(task_to_delete) == 0:
+        return 
+    elif int(task_to_delete) > len(task_list):
+        print("Chyba: Zadané číslo úkolu neexistuje.")
+    else:
+        task = task_list[int(task_to_delete)-1]
+        print("Úkol " + task["name"] + " byl odstraněn.")
+        del task_list[int(task_to_delete)-1]
     print()
-    return tasks
 
-tasks = []
-main(tasks)
+main()
